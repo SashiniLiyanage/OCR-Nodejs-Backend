e8d52de512a04eb4e302947e5338cd5ec979aa30
sashiniliyanage <e17190@eng.pdn.ac.lk>
2023-01-17 14:44:56 +0530

    minor changes

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,37 @@ const router = require('express').Router();
 const User = require('../models/User');
 const Request = require('../models/Request');
 const authenticateToken = require('../middleware/auth')
+const nodemailer = require('nodemailer')
 
+function sendEmail(){
+
+    return new Promise((resolve, reject)=>{
+
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth:{
+                user:'nimthara.liyanage@gmail.com',
+                pass:'sashini1003gmail'
+            }
+        })
+
+        const mail_config = {
+            from:'nimthara.liyanage@gmail.com',
+            to:'e17190@eng.pdn.ac.lk',
+            subject: 'OCRP Account is Ready to Use',
+            text:'Your OCRP account is created. Please login to the application using your credentials'
+        }
+
+        transporter.sendMail(mail_config, function(error, info){
+            if(error){
+                return reject({message: 'error sending emails'})
+            }
+
+            return resolve({message: 'email sent successfuly'})
+        })
+    })
+
+}
 // get all requests
 router.get('/requests', authenticateToken, async(req, res)=>{
     try{
@@ -68,9 +98,16 @@ router.post("/accept/:id", authenticateToken,async(req,res)=>{
                 const adduser = await newUser.save();
                 const {password,...others} = adduser._doc;
                 await Request.findByIdAndDelete(req.params.id)
+
+                sendEmail().then(response=>{
+                    others["message"] = "User registration successful!";
+                    return res.status(200).json(others);
+                }).catch(error =>{
+                    others["message"] = "Error: Email notification Failed. User registration successful!";
+                    return res.status(200).json(others);
+                })
                 
-                others["message"] = "User registration successful!";
-                return res.status(200).json(others);
+                
             }catch (error) {
                 return res.status(500).json({message:"User registration failed"});
             }
