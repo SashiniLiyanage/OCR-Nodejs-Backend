299a10c10f0dab5e92883adc81778db77526affa
denuwan55 <ldweerarathne5555@gmail.com>
2023-03-02 21:29:56 +0530

    .

--- models/Patient.js
+++ models/Patient.js
@@ -1,44 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const PatientSchema = new mongoose.Schema({
+const PatientSchema = new mongoose.Schema(
+  {
     patient_id: {
-        type: String,
-        required: true,
-      },
-      clinician_id: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-        required: true,
-      },
-      name: {
-        type: String,
-        required: true,
-      },
-      risk_factors: {
-        type: Array,
-        default: [],
-      },
-      DOB: {
-        type: Date,
-        required: true,
-      },
-      gender: {
-        type: String,
-        default: "",
-      },
-      histo_diagnosis: {
-        type: String,
-        default: "",
-      },
-      consent_form: {
-        type: String,
-        default: "",
-      },
-},
-{
+      type: String,
+      required: true,
+    },
+    clinician_id: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    risk_factors: {
+      type: Array,
+      default: [],
+    },
+    DOB: {
+      type: Date,
+      required: true,
+    },
+    gender: {
+      type: String,
+      default: "",
+    },
+    histo_diagnosis: {
+      type: String,
+      default: "",
+    },
+    consent_form: {
+      type: String,
+      default: "",
+    },
+  },
+  {
     versionKey: false,
-    timestamps:true
-}
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model("Patient",PatientSchema)
+module.exports = mongoose.model("Patient", PatientSchema);
