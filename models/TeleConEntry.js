cde4de9207433979b32c2c2a4d8721d8cfdde9bb
denuwan55 <ldweerarathne5555@gmail.com>
2023-03-02 21:45:24 +0530

    updated patient_id property in teleconentry

--- models/TeleConEntry.js
+++ models/TeleConEntry.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const TeleConEntrySchema = new mongoose.Schema(
   {
     patient_id: {
-      type: mongoose.Schema.ObjectId,
-      ref: "Patient",
+      type: String,
       required: true,
     },
     assignees: [
