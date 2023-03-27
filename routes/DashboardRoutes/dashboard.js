01e9c44c1b50338950b508c9f0dbb669213021ec
SRNethmi <e18282@eng.pdn.ac.lk>
2023-03-27 17:14:35 +0530

    add auth token in dashboard routes

--- routes/DashboardRoutes/dashboard.js
+++ routes/DashboardRoutes/dashboard.js
@@ -3,45 +3,54 @@ const User = require("../../models/User");
 const Image = require("../../models/Image");
 const Patient = require("../../models/Patient");
 
-const { authenticateToken, checkPermissions } = require("../../middleware/auth");
-
-
+const {
+  authenticateToken,
+  checkPermissions,
+} = require("../../middleware/auth");
 require("dotenv").config();
 
-// add the auth token
-router.get("/percentages", async (req, res) => {
- 
-  Patient.aggregate(
-    [
-      { $unwind: "$risk_factors" },
-      { $group: { _id: "$risk_factors.habit", count: { $sum: 1 } } },
-      { $project: { _id: 0, item: "$_id", count: 1 } },
-    ],
-    function (err, results) {
-      if (err) {
-        return res.send(err);
-      } else {
-        Patient.countDocuments({}, function (err, count) {
-          if (err) {
-            return res.send(err);
-          } else {
-            const arr = [];
-            results.forEach((result) => {
-              const percentage = (result.count / count) * 100;
-              arr.push(`${result.item}: ${percentage}%`);
-            });
+// GET route to get the % of risk habits
+router.get("/percentages", authenticateToken, async (req, res) => {
+  if (!checkPermissions(req.permissions, 100)) {
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
+  try {
+    Patient.aggregate(
+      [
+        { $unwind: "$risk_factors" },
+        { $group: { _id: "$risk_factors.habit", count: { $sum: 1 } } },
+        { $project: { _id: 0, item: "$_id", count: 1 } },
+      ],
+      function (err, results) {
+        if (err) {
+          return res.status(500).json(err);
+        } else {
+          Patient.countDocuments({}, function (err, count) {
+            if (err) {
+              return res.status(500).json(err);
+            } else {
+              const arr = [];
+              results.forEach((result) => {
+                const percentage = (result.count / count) * 100;
+                arr.push(`${result.item}: ${percentage}%`);
+              });
 
-            return res.send(arr);
-          }
-        });
+              return res.status(200).json(arr);
+            }
+          });
+        }
       }
-    }
-  );
+    );
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-
-// GET route to get the total number of doctors and patients
-router.get("/totals", async (req, res) => {
+// GET route to get the total number of doctors, images and patients
+router.get("/totals", authenticateToken, async (req, res) => {
+  if (!checkPermissions(req.permissions, 100)) {
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
   try {
     const patients = await Patient.countDocuments();
     const doctors = await User.countDocuments();
@@ -54,45 +63,5 @@ router.get("/totals", async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-// GET route to get the total number of doctors
-router.get("/doctors", async (req, res) => {
-  try {
-    User.countDocuments({}, function (err, count) {
-      if (err) throw err;
-
-      res.status(200).json({ count: count });
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// GET route to get the total number of doctors
-router.get("/patients", async (req, res) => {
-  try {
-    Patient.countDocuments({}, function (err, count) {
-      if (err) throw err;
-
-      res.status(200).json({ count: count });
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// GET route to get the total number of doctors
-router.get("/images", async (req, res) => {
-  try {
-    Image.countDocuments({}, function (err, count) {
-      if (err) throw err;
-
-      res.status(200).json({ count: count });
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 
 module.exports = router;
